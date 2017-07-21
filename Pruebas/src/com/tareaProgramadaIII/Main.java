708a426a0d7e6f7c6fc4b05a93c3ab142a25d540
SebastianCabrera <sebastiancz17@gmail.com>
2017-07-20 19:35:18 -0600

    Agregando cosas para la conexión

--- Pruebas/src/com/tareaProgramadaIII/Main.java
+++ Pruebas/src/com/tareaProgramadaIII/Main.java
@@ -1,8 +1,61 @@
 package com.tareaProgramadaIII;
 
+import java.math.BigInteger;
+import java.sql.*;
+
 public class Main {
 
+    //Me estoy basando en el ejemplo de la pagina:
+    //http://alvinalexander.com/java/java-mysql-select-query-example
+    public static void searchDB()
+    {
+        try
+        {
+            // create our mysql database connection
+            String myDriver = "org.gjt.mm.mysql.Driver"; //Si no es así es con com.mysql.jdbc.Driver
+            String myUrl = "jdbc:mysql://localhost/wiki"; //Siendo wiki el nombre de la base de datos
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "EstaSeriaUnaContrasena"); //
+
+            //Este seria el SELECT a implementar
+            String query = "SELECT * FROM page WHERE page_id = x"; //En x se pondría el id ingresado por el usuario
+            //String query = "SELECT * FROM page WHERE page_title = x"; //En x se pondría el nombre de la pagina ingresada por el usuario
+
+            // create the java statement
+            Statement st = conn.createStatement();
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+
+            // iterate through the java resultset
+            while (rs.next())
+            {
+                int id = rs.getInt("page_id");
+                int namespace = rs.getInt("page_namespace");
+                String title = rs.getString("page_title");
+                //Me salto page_restrictions por blob
+                int counter = rs.getInt("page_counter"); //En la base aparece como "bigint"
+                int is_redirect = rs.getInt("page_is_redirect"); //En la base aparece como "tinyint"
+                int is_new = rs.getInt("page_is_new"); //En la base aparece como "tinyint"
+                double random = rs.getDouble("page_random");
+                //Me salto page_touched por blob
+                int latest = rs.getInt("page_latest");
+                int len = rs.getInt("page_len");
+
+                // print the results
+                System.out.format("%s, %s, %s, %s, %s, %s, %s, %s, %s\n", id, namespace, title, counter, is_redirect, is_new, random, latest, len);
+            }
+            st.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-	System.out.println("Hello my baby, hello my honey");
+	    System.out.println("Hello my baby, hello my honey");
+	    searchDB();
     }
 }
