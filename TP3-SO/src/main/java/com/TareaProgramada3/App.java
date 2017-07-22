93572055d3f82518a338a9e20199baa81c8dcc50
SebastianCabrera <sebastiancz17@gmail.com>
2017-07-22 12:54:30 -0600

    Estaba haciendo pruebas de funcionalidad en App. Las comparto por ahora por cualquier cosa. Solo pruebas!

--- TP3-SO/src/main/java/com/TareaProgramada3/App.java
+++ TP3-SO/src/main/java/com/TareaProgramada3/App.java
@@ -4,13 +4,32 @@ package com.TareaProgramada3;
  * Hello world!
  *
  */
-public class App 
+public class App //Aqui solo se van a hacer pequeñas pruebas, no tiene nada que ver con el desarrollo de la aplicacion
 {
     public static void main( String[] args )
     {
         ObjectC objTmp = new ObjectC();
-        ListaCache tmp = new ListaCache();
-        tmp.clear();
+        //for(int i=0; i<=5; i++)
+        //{
+            objTmp.setKey(1);
+            objTmp.setValue("¡Felicidades!");
+        //}
+        String id = "Sebastian";
+        int cantidad = 8;
+        //ListaCache tmp = new ListaCache();
+        ListaCacheFIFO pruebaFIFO = new ListaCacheFIFO(cantidad, id);
+        pruebaFIFO.put(1, objTmp);
+        ObjectC respuesta = pruebaFIFO.get(1);
+        if(respuesta != null)
+        {
+            System.out.printf("El valor del objeto retornado es: " + respuesta.getValue() + "\n");
+        }
+        else
+        {
+            System.out.print("La direccion que busco esta vacia\n");
+        }
+
+        //pruebaFIFO.clear();
     }
 
 
