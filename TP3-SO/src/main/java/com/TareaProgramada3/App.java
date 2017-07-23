6d5f4c73408e90f39ab5badfd2874fb0e7f471ce
SebastianCabrera <sebastiancz17@gmail.com>
2017-07-22 23:37:16 -0600

    Una cosita pequeña de pruebas de LRU y FIFO

--- TP3-SO/src/main/java/com/TareaProgramada3/App.java
+++ TP3-SO/src/main/java/com/TareaProgramada3/App.java
@@ -4,66 +4,89 @@ package com.TareaProgramada3;
  * Hello world!
  *
  */
-public class App //Aqui solo se van a hacer pequeñas pruebas, no tiene nada que ver con el desarrollo de la aplicacion
+public class App //Se dejan unas pruebas para verificar el funcionamiento de los algoritmos
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        //Para prueba LRU
         ObjectC objTmp1 = new ObjectC();
-        //for(int i=0; i<=5; i++)
-        //{
-            objTmp1.setKey(1);
-            objTmp1.setValue("El primero");
-        //}
+        objTmp1.setKey(1);
+        objTmp1.setValue("El primero");
+
         ObjectC objTmp2 = new ObjectC();
-        objTmp2.setKey(4);
+        objTmp2.setKey(10);
         objTmp2.setValue("El segundo");
 
         ObjectC objTmp3 = new ObjectC();
-        objTmp3.setKey(2);
+        objTmp3.setKey(15);
         objTmp3.setValue("El tercero");
 
         ObjectC objTmp4 = new ObjectC();
-        objTmp4.setKey(7);
+        objTmp4.setKey(20);
         objTmp4.setValue("El cuarto");
 
-        String id = "Sebastian";
+        String id = "Sebastian1";
         int cantidad = 3;
-        ListaCache prueba;
-        prueba = new ListaCacheLRU(cantidad, id);
-        prueba.put(1, objTmp1);
-        prueba.put(4, objTmp2);
-        prueba.put(2, objTmp3);
-        ObjectC respuesta = prueba.get(4);
-        if(respuesta != null)
-        {
+        ListaCache prueba1;
+        prueba1 = new ListaCacheLRU(cantidad, id);
+        prueba1.put(5, objTmp1);
+        prueba1.put(10, objTmp2);
+        prueba1.put(15, objTmp3);
+        ObjectC respuesta = prueba1.get(10);
+        if (respuesta != null) {
             System.out.printf("El valor del objeto retornado es: " + respuesta.getValue() + "\n");
-        }
-        else
-        {
+        } else {
             System.out.print("La direccion que buscó esta vacia\n");
         }
-        respuesta = prueba.get(1);
-        if(respuesta != null)
-        {
+        respuesta = prueba1.get(5);
+        if (respuesta != null) {
             System.out.printf("El valor del objeto retornado es: " + respuesta.getValue() + "\n");
-        }
-        else
-        {
+        } else {
             System.out.print("La direccion que buscó esta vacia\n");
         }
-        prueba.put(7, objTmp4);
-        respuesta = prueba.get(2); //AQUI LOS CAMBIOS
-        //System.out.print(prueba.list.get(3).getValue() + "\n");
-        if(respuesta != null)
-        {
+        prueba1.put(20, objTmp4);
+        respuesta = prueba1.get(15); //En una prueba exitosa, este get deberia recibir un valor null
+        if (respuesta != null) {
             System.out.printf("El valor del objeto retornado es: " + respuesta.getValue() + "\n");
-        }
-        else
-        {
+        } else {
             System.out.print("La direccion que buscó esta vacia\n");
         }
 
-        prueba.clear();
+        prueba1.clear();
+
+
+        //Para prueba FIFO
+        ObjectC objTmp5 = new ObjectC();
+        objTmp1.setKey(25);
+        objTmp1.setValue("El primero");
+
+        ObjectC objTmp6 = new ObjectC();
+        objTmp2.setKey(30);
+        objTmp2.setValue("El segundo");
+
+        ObjectC objTmp7 = new ObjectC();
+        objTmp3.setKey(35);
+        objTmp3.setValue("El tercero");
+
+        ObjectC objTmp8 = new ObjectC();
+        objTmp4.setKey(40);
+        objTmp4.setValue("El cuarto");
+        id = "Sebastian2";
+        ListaCache prueba2;
+        prueba2 = new ListaCacheFIFO(cantidad, id);
+        prueba2.put(25, objTmp5);
+        prueba2.put(30, objTmp6);
+        prueba2.put(35, objTmp7);
+        prueba2.put(40, objTmp8);
+        respuesta = prueba2.get(25); //En una prueba exitosa, este get deberia recibir un valor null
+        if (respuesta != null) {
+            System.out.printf("El valor del objeto retornado es: " + respuesta.getValue() + "\n");
+        } else {
+            System.out.print("La direccion que buscó esta vacia\n");
+        }
+
+        prueba2.clear();
+
+        
     }
 
 
