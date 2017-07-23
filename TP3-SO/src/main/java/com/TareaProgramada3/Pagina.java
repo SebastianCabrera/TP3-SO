f5c85b6aae750f3158cb13dfa963c8664a21cf0d
sebastianVUCR <sebastian.vargas.99@gmail.com>
2017-07-22 20:04:27 -0600

    Clase pagina (gets y sets)
    no sé por qué sale una lista tan grande de modificaciones en los archivo

---
+++ TP3-SO/src/main/java/com/TareaProgramada3/Pagina.java
@@ -0,0 +1,97 @@
+package com.TareaProgramada3;
+
+/**
+ * Created by sebastian on 22/7/2017.
+ */
+public class Pagina
+{
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNamespace() {
+        return namespace;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public int getIs_redirect() {
+        return is_redirect;
+    }
+
+    public int getIs_new() {
+        return is_new;
+    }
+
+    public double getRandom() {
+        return random;
+    }
+
+    public int getLatest() {
+        return latest;
+    }
+
+    public int getLen() {
+        return len;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNamespace(int namespace) {
+        this.namespace = namespace;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public void setIs_redirect(int is_redirect) {
+        this.is_redirect = is_redirect;
+    }
+
+    public void setIs_new(int is_new) {
+        this.is_new = is_new;
+    }
+
+    public void setRandom(double random) {
+        this.random = random;
+    }
+
+    public void setLatest(int latest) {
+        this.latest = latest;
+    }
+
+    public void setLen(int len) {
+        this.len = len;
+    }
+
+    int id;
+    int namespace;
+    String title;
+    int counter;
+    int is_redirect;
+    int is_new;
+    double random;
+    int latest;
+    int len;
+
+    Pagina()
+    {
+
+    }
+
+}
